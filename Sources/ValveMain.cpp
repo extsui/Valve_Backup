e74ffc2f11869032d896975a945de94730261f01
extsui <en422641@s.okayama-u.ac.jp>
2021-07-25 12:49:06 +0900

    4ch 分のポート設定を追加

--- Sources/ValveMain.cpp
+++ Sources/ValveMain.cpp
@@ -11,6 +11,36 @@ constexpr int RotaryEncoderCount = 4;
 
 RotaryEncoder g_RotaryEncoder[RotaryEncoderCount];
 
+struct RotaryEncoderPortPin
+{
+    PortPin PhaseA;
+    PortPin PhaseB;
+};
+
+RotaryEncoderPortPin RotaryEncoderPortPinSettings[] = 
+{
+    // ch1
+    {
+        PortPin(RotaryEncoder1A_GPIO_Port, RotaryEncoder1A_Pin),
+        PortPin(RotaryEncoder1B_GPIO_Port, RotaryEncoder1B_Pin),
+    },
+    // ch2
+    {
+        PortPin(RotaryEncoder2A_GPIO_Port, RotaryEncoder2A_Pin),
+        PortPin(RotaryEncoder2B_GPIO_Port, RotaryEncoder2B_Pin),
+    },
+    // ch3
+    {
+        PortPin(RotaryEncoder3A_GPIO_Port, RotaryEncoder3A_Pin),
+        PortPin(RotaryEncoder3B_GPIO_Port, RotaryEncoder3B_Pin),
+    },
+    // ch4
+    {
+        PortPin(RotaryEncoder4A_GPIO_Port, RotaryEncoder4A_Pin),
+        PortPin(RotaryEncoder4B_GPIO_Port, RotaryEncoder4B_Pin),
+    }
+};
+
 }
 
 void ValveMain()
@@ -18,11 +48,12 @@ void ValveMain()
     Console::SetPort(&huart2);
     Console::Log("Valve Start.\n");
 
-    RotaryEncoder& rotaryEncoder = g_RotaryEncoder[0];
+    for (int i = 0; i < RotaryEncoderCount; i++) {
+        auto setting = &RotaryEncoderPortPinSettings[i];
+        g_RotaryEncoder[i].SetPortPin(&setting->PhaseA, &setting->PhaseB);
+    }
 
-    PortPin a(RotaryEncoder1A_GPIO_Port, RotaryEncoder1A_Pin);
-    PortPin b(RotaryEncoder1B_GPIO_Port, RotaryEncoder1B_Pin);
-    rotaryEncoder.SetPortPin(&a, &b);
+    RotaryEncoder& rotaryEncoder = g_RotaryEncoder[0];
     rotaryEncoder.Sample();
 
     int samplingCount = 0;
